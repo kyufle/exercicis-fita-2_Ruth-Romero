2d394139e8e510428756395ab55f7b0d27e4c75f
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2025-09-29 10:29:01 +0200

    creation of action page and html generated

--- form.php
+++ form.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User form</title>
+    <title>User form - Ruth Romero</title>
 </head>
 <body>
     <form action="post" action=/action_page.php>
