b57279e0e0fb6c9c1f6e26ba58b86b455c828c04
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2025-09-29 10:26:53 +0200

    creation of  php form

---
+++ form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User form</title>
+</head>
+<body>
+    <form action="post" action=/action_page.php>
+        <label for="Name">Name: <input id="usuari" type="text"></label>
+        <label for="Password">Password: <input id="contrasenya" type="password"></label>
+        <input type="submit">
+    </form>
+</body>
+</html>
